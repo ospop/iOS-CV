3e2ac01549dbf15fd09a730282ed426647f2a5bf
pillkaris <dragos042@yahoo.com>
2021-02-24 16:34:04 +0200

    before media queries

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 // console.log(`linked`);
 
+const linkedIn = document.getElementById('linkedin');
+const gitHub = document.getElementById('github');
 const switchBtn = document.querySelectorAll('.see--more--btn');
 const cardSkills1 = document.querySelector('.card--skills');
 const cardSkills2 = document.querySelector('.card--skills-2');
@@ -16,3 +18,23 @@ const showLess = function () {
 
 switchBtn[0].addEventListener('click', cardSwitch);
 switchBtn[1].addEventListener('click', cardSwitch);
+
+linkedIn.addEventListener('click', () => {
+  window.open('https://www.linkedin.com/in/dragos-popescu-85a0921ab/');
+});
+
+gitHub.addEventListener('click', () => {
+  window.open('https://github.com/ospop');
+});
+
+let today = new Date();
+
+function addZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
+let hours = addZero(today.getHours());
+let minutes = addZero(today.getMinutes());
+let currentTime = `${hours}:${minutes}`;
+
+document.getElementById('time').innerHTML = currentTime;
