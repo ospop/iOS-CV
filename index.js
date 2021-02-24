b43243364dbf20add496aadc177d7777c6bed35c
pillkaris <dragos042@yahoo.com>
2021-02-24 11:29:12 +0200

    progress
    added stacking cards effect,
    work portal sites

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 'use strict';
-console.log(`linked`);
+// console.log(`linked`);
+
+const switchBtn = document.querySelectorAll('.see--more--btn');
+const cardSkills1 = document.querySelector('.card--skills');
+const cardSkills2 = document.querySelector('.card--skills-2');
+
+const cardSwitch = function () {
+  cardSkills1.classList.toggle('card--hidden');
+  cardSkills2.classList.toggle('card--hidden');
+};
+
+const showLess = function () {
+  xpCard.classList.toggle('folded');
+};
+
+switchBtn[0].addEventListener('click', cardSwitch);
+switchBtn[1].addEventListener('click', cardSwitch);
